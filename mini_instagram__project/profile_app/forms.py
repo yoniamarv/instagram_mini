e2c4389853f81f571b8f32b35be4a9345fec41a3
yoni <yoni.amarv@gmail.com>
2019-01-23 13:39:28 +0200

    gui signup / login

--- mini_instagram__project/profile_app/forms.py
+++ mini_instagram__project/profile_app/forms.py
@@ -41,6 +41,7 @@ class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+        help_texts = {'username':None}
         widgets = {
             'username': forms.TextInput(attrs={
                 'id': 'login-username',
